8cd4c4d8dd58b15ff176280f6d27161e9d05da2e
edificio-alto <kirigawa.kirito.sao@gmail.com>
2025-11-12 00:20:46 +0100

    pepe

---
+++ Source/Mannaggia/Private/cubbi/cubbo.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Public/cubbi/cubbo.h"
+
+
+// Sets default values
+Acubbo::Acubbo()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	CubeMesh = CreateDefaultSubobject<UStaticMeshComponent>("mimmo")
+	RootComponent = CubeMesh;
+	
+	CubeMesh -> SetSimulatePhysics(true)
+	CubeMesh -> SetCollisionProfileName()
+	CubeMesh -> SetCollisionResponseToChannel();
+	CubeMesh -> SetGenerateOverlapEvents(true)
+}
+
+// Called when the game starts or when spawned
+void Acubbo::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void Acubbo::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/Mannaggia/Public/cubbi/cubbo.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "cubbo.generated.h"
+
+UCLASS()
+class MANNAGGIA_API Acubbo : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	Acubbo();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<class UStaticMeshComponent> CubeMesh;
+
+	int32 GetScoreValue() const {return ScoreValue;}
+
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditAnywhere, Category = "Points")
+	int32 ScoreValue = 10;
+};
+	
